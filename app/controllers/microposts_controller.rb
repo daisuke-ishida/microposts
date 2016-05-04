a0b49e31b2ceeef5fe48e11f9bfd1c1203193c76
daisukeishida <berg7295karajan@gmail.com>
2016-05-04 14:54:33 +0000

    add positing microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,8 @@ class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create]
     
     def create
-        @microposts = current_user.microposts.build(microposts_params)
-        if microposts.save
+        @micropost = current_user.microposts.build(micropost_params)
+        if @micropost.save
             flash[:success] = "Microposts created!"
             redirect_to root_url
         else
@@ -11,8 +11,16 @@ class MicropostsController < ApplicationController
         end
     end
     
+    def destroy
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        return redirect_to root_url if @micropost.nil?
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
+    end
+    
     private
-    def microposts_params
-        params.reqire(:microposts).permit(:content)
+    def micropost_params
+        params.require(:micropost).permit(:content)
     end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,13 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
->>>>>>> user-profile
-=======
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
->>>>>>> user-profile
 end
