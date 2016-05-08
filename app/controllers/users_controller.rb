8d92a7652ae1083a39b7316ae9c1f237c830da9b
daisukeishida <berg7295karajan@gmail.com>
2016-05-08 07:41:08 +0000

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,8 @@ class UsersController < ApplicationController
   end
 
 def update
-  @user = User.find(prams[:id])
-  if @user.update_attribute(user_params)
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
     flash[:success] = "Profile updated!"
     redirect_to @user
   else
